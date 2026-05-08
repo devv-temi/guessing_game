e19e0a2887423d743759979d574bb90e7ac98d1d
devv-temi <balogunjames012@gmail.com>
2026-05-08 22:41:46 +0800

    rust commit

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    let guess
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("too small!"),
+        Ordering::Greater => println!("too big!"),
+        Ordering::Equal => println!("you winn!"),
+    }
+
+    println!("The secret number is: {secret_number}");
+}
